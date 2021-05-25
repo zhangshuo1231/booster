fca71206e918dd872d8a8028552d7cd710a7d0c5
ILS Service <ils@solutions.com>
2021-05-24 21:03:49 -0400

    Progress.

--- api/controllers/hello.js
+++ api/controllers/hello.js
@@ -1,7 +1,23 @@
+const bunyan = require('bunyan')
+const log = bunyan.createLogger({name: "booster"});
+const Swagger = require("../../lib/swagger.js");
+const clients = require('../../lib/persistence/model/client')
+
 module.exports = {
-    greeting
+    greeting,
+    addClients
 };
 
 function greeting(req, res) {
+    log.info("Entering greeting method")
     res.status(200).send('Hi Shawn!');
+    log.info("Exiting greeting method")
+}
+
+async function addClients(req, res) {
+    log.info("Entering addClient method")
+    const jsonObj = Swagger.getBody(req);
+    const result = await clients.insertMany(jsonObj);
+    res.status(200).send(result);
+    log.info("Exiting addClient method")
 }
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const app = require("express")();
 const SwaggerExpress = require("swagger-express-mw");
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost:27017/Booster'
+const PORT = '10050'
 
 const config = {
     appRoot: __dirname, // required config
@@ -12,5 +15,11 @@ SwaggerExpress.create(config, function (err, swaggerExpress) {
     swaggerExpress.register(app);
 });
 
-app.listen('10050');
-console.log('Ready to serve you, sir')
+mongoose.connect(uri,{ useUnifiedTopology: true, useNewUrlParser: true})
+const connection = mongoose.connection
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`)
